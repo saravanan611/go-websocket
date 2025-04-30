6f72bd4ffeb7889d539f276730f1c02db609dd1e
saravanan selvam <saravanan9944gs@gmail.com>
2025-04-30 07:23:36 +0530

    fix a redirect bug

--- main.go
+++ main.go
@@ -18,6 +18,9 @@ func main() {
 		log.Fatalf("Error in generating self-signed certificate: %v", err)
 	}
 
+	// fmt.Println("Certificate file:", certFile)
+	// fmt.Println("Key file:", keyFile)
+
 	base.GPortNo = "8443" // HTTPS port
 
 	// Initialize Gin router
@@ -38,12 +41,12 @@ func main() {
 		wsserver.GetData(c.Writer, c.Request)
 	})
 
-	// r.NoRoute(func(c *gin.Context) {
-	// 	c.Redirect(http.StatusMovedPermanently, base.GBaseAPI+"/")
-	// })
+	r.NoRoute(func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, base.GBaseAPI+"/")
+	})
 
 	// Serve a test chat HTML page
-	r.GET(base.GBaseAPI+"/testchat", func(c *gin.Context) {
+	r.GET(base.GBaseAPI+"/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "chate.html", gin.H{})
 	})
 
@@ -51,7 +54,11 @@ func main() {
 	r.LoadHTMLGlob("html/*")
 
 	// Start the server with TLS
-	log.Printf("WebSocket server is running at https://localhost:%s...", base.GPortNo)
+	log.Printf("WebSocket server is running at http://localhost:%s...", base.GPortNo)
+
+	// if err := r.Run(":" + base.GPortNo); err != nil {
+	// 	log.Fatalf("Server error: %v", err)
+	// }
 	if err := r.RunTLS(fmt.Sprintf(":%s", base.GPortNo), certFile, keyFile); err != nil {
 		log.Fatalf("Server error: %v", err)
 	}
